2ad3c7f0565b8f98d7dd22c663e3eea60520ff50
arun-gokul <arungokul.g.r.2015.cse@ritchennai.edu.in>
2023-12-11 13:11:34 +0530

    codelib secret key handling

--- functions/zohocrm_bulk_callback/index.js
+++ functions/zohocrm_bulk_callback/index.js
@@ -14,51 +14,39 @@ app.post("/read", async (req, res) => {
 app.post("/write", async (req, res) => {
   console.log(req.body);
   let data = req.body;
-  if (data.state == "COMPLETED") {
-    let jobId = data.job_id;
-    let operation = data.operation;
-    let downloadURL = data.result.download_url;
-    let page = data.result.page;
-    let newPage = page;
-    downloadURL = "https://zohoapis.com" + downloadURL;
-    let more_records = data.result.more_records;
-    console.log(more_records);
-    if (more_records) {
-      newPage = newPage + 1;
+  if (req.header.CODELIB_SECRET_KEY === process.env[CODELIB_SECRET_KEY]) {
+    if (data.state == "COMPLETED") {
+      let jobId = data.job_id;
+      let operation = data.operation;
+      let downloadURL = data.result.download_url;
+      let page = data.result.page;
+      let newPage = page;
+      downloadURL = "https://zohoapis.com" + downloadURL;
+      let more_records = data.result.more_records;
+      console.log(more_records);
+      if (more_records) {
+        newPage = newPage + 1;
+      }
+      const catalystApp = catalyst.initialize(req);
+      if (operation == "read") {
+        let res = await catalystApp
+          .zcql()
+          .executeZCQLQuery(
+            "update BulkRead set DOWNLOAD_URL='" +
+              downloadURL +
+              "',REQUESTED_PAGE_NO='" +
+              newPage +
+              "' where CRMJOBID='" +
+              jobId +
+              "'"
+          );
+        console.log(res);
+      }
     }
-    const catalystApp = catalyst.initialize(req);
-    if (operation == "read") {
-      let res = await catalystApp
-        .zcql()
-        .executeZCQLQuery(
-          "update BulkRead set DOWNLOAD_URL='" +
-            downloadURL +
-            "',REQUESTED_PAGE_NO='" +
-            newPage +
-            "' where CRMJOBID='" +
-            jobId +
-            "'"
-        );
-      console.log(res);
-    }
+    res.status(200);
+  } else {
+    res.status(401).send("You are unauthorised to perform this action");
   }
-  res.status(200).send("I am Live and Ready.");
 });
 
 module.exports = app;
-
-//Create a JSON object for adding a new user
-const signupConfig = {
-  platform_type: "web",
-  template_details: {
-    senders_mail: "dogogetu@tutuapp.bid",
-    subject: "Welcome to %APP_NAME% ",
-    message: "<p>Hello ,</p> <p>Follow this link to join in %APP_NAME% .</p>",
-  },
-};
-var userConfig = {
-  first_name: "Dannie",
-  last_name: "Boyle",
-  email_id: "p.boyle@zylker.com",
-  role_id: "3376000000159024",
-};
